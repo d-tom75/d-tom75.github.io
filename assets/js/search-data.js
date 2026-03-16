1bcf59e458e13e2fe2eee5b9da219b5ce86e86a2
d-tom75 <109771200+d-tom75@users.noreply.github.com>
2026-03-16 17:03:52 +0000

    Deploying to gh-pages from @ d-tom75/d-tom75.github.io@58c7c6b0529e32903c032ba916904daf874d3170 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -457,7 +457,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/curriculum_vitae.pdf", "_blank");
         },
       },{
         id: 'social-email',
